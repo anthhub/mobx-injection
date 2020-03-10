b7258e8a458a2daa97a65d2da2cfc1c6dadfd751
qiyuan.tang01 <qiyuan.tang01@liulishuo.com>
2020-03-10 16:46:04 +0800

    [build] 0.1.0

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,7 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
 
 const pkg = require('./package.json')
 
@@ -16,7 +17,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['react'], 
   watch: {
     include: 'src/**',
   },
@@ -27,6 +28,11 @@ export default {
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
+
+    babel({
+      exclude: 'node_modules/**' // 只编译我们的源代码
+    }),
+
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
